712406d4cf49778b462d9ed9ff458f7629d44d3a
Ripesh Khadka <khadkanripesh@gmail.com>
2025-02-18 16:16:53 +0545

    declare types for svgr

--- apps/joinwrite-enduser/src/utils/toast.ts
+++ apps/joinwrite-enduser/src/utils/toast.ts
@@ -1,6 +1,6 @@
 import { toast } from 'react-toastify';
-// import Check from '../assets/icons/Check Circle.svg?react';
+import Check from '../assets/icons/Check.svg?react';
 
 export const toastError = (message: string) => {
-  toast(message, { type: 'error' });
+  toast(message, { type: 'error', icon: Check });
 };
